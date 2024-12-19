4cfc63065434244bdd10ccd977b62ebcf0af2d6e
rodrg <rodrgsassuncao@gmail.com>
2024-12-19 22:05:13 +0000

    Algumas melhorias e recuperação de info kotlin

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.myapplication
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -16,7 +18,27 @@ class MainActivity : AppCompatActivity() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+            insets }
+
+
+
+            val btnClean: Button = findViewById (R.id.btn_clean)
+            val btnCalcular: Button = findViewById(R.id.btn_done)
+            val edtValorTotal: TextInputEditText = findViewById(R.id.valor_conta)
+            val edtNumpeople: TextInputEditText = findViewById(R.id.til_num_people)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
-}
